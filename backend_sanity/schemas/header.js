8ab48160f41ba12252ced5a3b983c6ce8102956c
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2024-01-25 01:48:38 +0530

    adding top skill section in header

--- backend_sanity/schemas/header.js
+++ backend_sanity/schemas/header.js
@@ -1,25 +1,40 @@
 export default {
-    name: 'header',
-    title: 'Header',
-    type: 'document',
-    fields: [
+  name: 'header',
+  title: 'Header',
+  type: 'document',
+  fields: [
+    {
+      name: 'mainName',
+      title: 'Main Name',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'headerMainImage',
+      title: 'Profile Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'headerSkill',
+      title: 'Header Skill',
+      type: 'array',
+      of: [
         {
-            name: 'mainName',
-            title: 'Main Name',
-            type: 'string',
+          name: 'icon',
+          title: 'Icon',
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
         },
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'string',
-        },
-        {
-            name: 'headerMainImage',
-            title: 'Profile Image',
-            type: 'image',
-            options: {
-                hotspot: true,
-            },
-        },
-    ],
-};
+      ],
+    },
+  ],
+}
